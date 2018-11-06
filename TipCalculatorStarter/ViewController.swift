121c62252f4e3b5a3497597aeccdf0b9179bb542
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-11-06 10:42:03 -0800

    Created the input card view

--- TipCalculatorStarter/ViewController.swift
+++ TipCalculatorStarter/ViewController.swift
@@ -9,14 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - Header Outlets of calculator
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
     
-    // MARK: - View Lifecycle
+    // MARK: - Input Card Outlets of calculator
+    @IBOutlet weak var inputCardView: UIView!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentSegmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func themeToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            print("switch toggle on")
+        } else {
+            print("switch toggle is off")
+        }
+    }
+    
+    @IBAction func tipPercentChanged(_ sender: UISegmentedControl) {
     }
 }
 
